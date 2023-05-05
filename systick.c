da472e7008aacbc0ff247931db9c502214b18c7c
Youssef Emad <109358115+Y-Emad@users.noreply.github.com>
2023-05-05 23:42:43 +0300

    Update systick.c
    both delayInSeconds() & delay() exist now

--- systick.c
+++ systick.c
@@ -7,6 +7,15 @@ void initSystick() {
 	NVIC_ST_CTRL_R = NVIC_ST_CTRL_ENABLE | NVIC_ST_CTRL_CLK_SRC ;
 }
 
+void delayInSeconds(int total) {
+	int i;
+	for (i=0; i < total; i++) {
+		NVIC_ST_RELOAD_R = (16000000) - 1;
+		NVIC_ST_CURRENT_R = 0X00;
+		while ((NVIC_ST_CTRL_R & NVIC_ST_CTRL_COUNT) == 0x00){}
+	}
+}
+
 void delay(int total) {
 	int i;
 	for (i=0; i < total; i++) {
