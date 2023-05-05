50ebbec95531ad79f2c3a61eb7b43929dce34a30
Ali Mohsen <ali_m_2001@hotmail.com>
2023-05-05 18:10:25 +0300

    added systick initialization c file

---
+++ systick.c
@@ -0,0 +1,17 @@
+#include "declarations.h"
+
+void initSystick() {
+	NVIC_ST_CTRL_R = 0X00;
+	NVIC_ST_RELOAD_R = NVIC_ST_RELOAD_M;
+	NVIC_ST_CURRENT_R = 0X00;
+	NVIC_ST_CTRL_R = NVIC_ST_CTRL_ENABLE | NVIC_ST_CTRL_CLK_SRC ;
+}
+
+void delayInSeconds(int total) {
+	int i;
+	for (i=0; i < total; i++) {
+		NVIC_ST_RELOAD_R = (16000000) - 1;
+		NVIC_ST_CURRENT_R = 0X00;
+		while ((NVIC_ST_CTRL_R & NVIC_ST_CTRL_COUNT) == 0x00){}
+	}
+}
